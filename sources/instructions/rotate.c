320445097a155c389e376a911f437f958387e278
Piesito <83318869+dejapiunrato@users.noreply.github.com>
2024-12-24 10:51:47 +0100

    Update rotate.c

--- sources/instructions/rotate.c
+++ sources/instructions/rotate.c
@@ -14,21 +14,21 @@ void	rotate(t_stack	**stack)
 	last->next = tmp;
 }
 
-void	rotate_a(t_stack **stack)
+char	*rotate_a(t_stack **stack)
 {
 	rotate(stack);
-	ft_putstr("ra\n");
+	return ("ra\n");
 }
 
-void	rotate_b(t_stack **stack)
+char	*rotate_b(t_stack **stack)
 {
 	rotate(stack);
-	ft_putstr("rb\n");
+	return ("rb\n");
 }
 
-void	rotate_ab(t_stack **stack_a, t_stack **stack_b)
+char	*rotate_ab(t_stack **stack_a, t_stack **stack_b)
 {
 	rotate(stack_a);
 	rotate(stack_b);
-	ft_putstr("rr\n");
+	return ("rr\n");
 }
